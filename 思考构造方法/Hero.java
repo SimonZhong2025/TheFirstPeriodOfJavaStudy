c868aeb98963180f6168f21826ad98013b047df0
SimonZhong <1109267330@qq.com>
2019-09-11 23:36:07 +0800

    “有void 和无void”“引用传递”
    已经解决引用传递的问题，还有有void方法和无void的构造方法的问题没解决

--- 思考构造方法/Hero.java
+++ 思考构造方法/Hero.java
@@ -8,12 +8,13 @@ class Heroric {
 }
 
 public class Hero extends Heroric {
+//有void和无void区别
 	public void attack() {
 		System.out.println(name + " 进行了一次攻击 ，但是不确定打中谁了");
 	}
 
 //为什么这里是Hero 而不是String，为什么下面是name而不是bh.name???
-//有void和无void区别
+	
 	public void attack(Hero h1) {
 		System.out.println(name + "对" + h1.name + "进行了一次攻击 ");
 	}
@@ -39,3 +40,7 @@ public class Hero extends Heroric {
 
 /*这里用到的是引用传递的方法，记住这个格式。引用传递可以引用到实例中的全部属性。这个比单纯的值传递的作用大一些，但各有好处，值得注意的是bh.attack。
  *也就是L18和L22的name是this.Hero.name也就是bh的name。所以输出结果就是“赏金猎人”
+ *这里name传递过去是引用，而（String name）的值传递传递的是基本类型的值，性质是不一样的
+ */
+
+/*
